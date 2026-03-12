763f7a24fa2603632923c897680669ac642a7bd0
Christoph Stein <christoph.werner.stein@gmail.com>
2026-03-12 11:19:05 +0100

    Fix wrong parameter value

--- slurm/generate_experiment_scripts.py
+++ slurm/generate_experiment_scripts.py
@@ -29,8 +29,8 @@ with open("run_experiments.sh", "w") as target_file:
         target_file.write(experiment_cmd)
 
         # Mutation only
-        experiment_cmd = f"sbatch --job-name=exp_0.4mp0.0cp_one-point_{seed}s"
-        experiment_cmd += (f" --export=crossover=\"one-point\",mutation_prob=0.4,crossover_prob=0.0,"
+        experiment_cmd = f"sbatch --job-name=exp_0.04mp0.0cp_one-point_{seed}s"
+        experiment_cmd += (f" --export=crossover=\"one-point\",mutation_prob=0.04,crossover_prob=0.0,"
                             f"seed={seed},result_dir=/home/ws/ws16/CEIQ/results,generations={generations},target=qft,tag=experiment")
         experiment_cmd += f" run_experiment.sh\n"
 
