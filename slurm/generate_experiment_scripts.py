f0de23b52f461c2e194c5df65bad7a8ae1f8fd6a
Christoph Stein <christoph.werner.stein@gmail.com>
2026-03-12 13:22:26 +0100

    Fix missing quotation marks

--- slurm/generate_experiment_scripts.py
+++ slurm/generate_experiment_scripts.py
@@ -6,6 +6,7 @@ crossover_prob = 0.7
 
 generations = 50_000
 
+# Adjust tag if different target is used.
 tag = "experiment"
 
 with open("slurm/run_experiments.sh", "w") as target_file:
@@ -17,7 +18,7 @@ with open("slurm/run_experiments.sh", "w") as target_file:
 
             experiment_cmd = f"sbatch --job-name=exp_{mutation_prob}mp{crossover_prob}cp_{crossover}_{seed}s"
             experiment_cmd += (f" --export=crossover=\"{crossover}\",mutation_prob={mutation_prob},crossover_prob={crossover_prob},"
-                               f"seed={seed},result_dir=/home/ws/ws16/CEIQ/results,generations={generations},target=qft,tag={tag}")
+                               f"seed={seed},result_dir=/home/ws/ws16/CEIQ/results,generations={generations},target=qft,tag=\"{tag}\"")
             experiment_cmd += f" run_experiment.sh\n"
 
             target_file.write(experiment_cmd)
@@ -25,7 +26,7 @@ with open("slurm/run_experiments.sh", "w") as target_file:
         # Random search
         experiment_cmd = f"sbatch --job-name=exp_1.0mp0.0cp_one-point_{seed}s"
         experiment_cmd += (f" --export=crossover=\"one-point\",mutation_prob=1.0,crossover_prob=0.0,"
-                            f"seed={seed},result_dir=/home/ws/ws16/CEIQ/results,generations={generations},target=qft,tag={tag}")
+                            f"seed={seed},result_dir=/home/ws/ws16/CEIQ/results,generations={generations},target=qft,tag=\"{tag}\"")
         experiment_cmd += f" run_experiment.sh\n"
 
         target_file.write(experiment_cmd)
@@ -33,7 +34,7 @@ with open("slurm/run_experiments.sh", "w") as target_file:
         # Mutation only
         experiment_cmd = f"sbatch --job-name=exp_0.04mp0.0cp_one-point_{seed}s"
         experiment_cmd += (f" --export=crossover=\"one-point\",mutation_prob=0.04,crossover_prob=0.0,"
-                            f"seed={seed},result_dir=/home/ws/ws16/CEIQ/results,generations={generations},target=qft,tag={tag}")
+                            f"seed={seed},result_dir=/home/ws/ws16/CEIQ/results,generations={generations},target=qft,tag=\"{tag}\"")
         experiment_cmd += f" run_experiment.sh\n"
 
         target_file.write(experiment_cmd)
