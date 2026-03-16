e75eb7d603d8f35f417232efe523194582455de8
Christoph Stein <christoph.werner.stein@gmail.com>
2026-03-16 09:29:36 +0100

    Change experiment parameters

--- slurm/generate_experiment_scripts.py
+++ slurm/generate_experiment_scripts.py
@@ -1,13 +1,15 @@
-seed_num = 15
-seed_offset = 0
+seed_num = 30
+seed_offset = 30
 
 mutation_prob = 0.04
 crossover_prob = 0.7
 
-generations = 50_000
+generations = 20_000
 
 # Adjust tag if different target is used.
-tag = "experiment"
+# target = "qft"
+target = "qft"
+tag = f"{target}_experiment"
 
 with open("slurm/run_experiments.sh", "w") as target_file:
 
@@ -18,7 +20,7 @@ with open("slurm/run_experiments.sh", "w") as target_file:
 
             experiment_cmd = f"sbatch --job-name=exp_{mutation_prob}mp{crossover_prob}cp_{crossover}_{seed}s"
             experiment_cmd += (f" --export=crossover=\"{crossover}\",mutation_prob={mutation_prob},crossover_prob={crossover_prob},"
-                               f"seed={seed},result_dir=/home/ws/ws16/CEIQ/results,generations={generations},target=qft,tag=\"{tag}\"")
+                               f"seed={seed},result_dir=/home/ws/ws16/CEIQ/results,generations={generations},target=\"{target}\",tag=\"{tag}\"")
             experiment_cmd += f" run_experiment.sh\n"
 
             target_file.write(experiment_cmd)
@@ -26,7 +28,7 @@ with open("slurm/run_experiments.sh", "w") as target_file:
         # Random search
         experiment_cmd = f"sbatch --job-name=exp_1.0mp0.0cp_one-point_{seed}s"
         experiment_cmd += (f" --export=crossover=\"one-point\",mutation_prob=1.0,crossover_prob=0.0,"
-                            f"seed={seed},result_dir=/home/ws/ws16/CEIQ/results,generations={generations},target=qft,tag=\"{tag}\"")
+                            f"seed={seed},result_dir=/home/ws/ws16/CEIQ/results,generations={generations},target=\"{target}\",tag=\"{tag}\"")
         experiment_cmd += f" run_experiment.sh\n"
 
         target_file.write(experiment_cmd)
@@ -34,7 +36,7 @@ with open("slurm/run_experiments.sh", "w") as target_file:
         # Mutation only
         experiment_cmd = f"sbatch --job-name=exp_0.04mp0.0cp_one-point_{seed}s"
         experiment_cmd += (f" --export=crossover=\"one-point\",mutation_prob=0.04,crossover_prob=0.0,"
-                            f"seed={seed},result_dir=/home/ws/ws16/CEIQ/results,generations={generations},target=qft,tag=\"{tag}\"")
+                            f"seed={seed},result_dir=/home/ws/ws16/CEIQ/results,generations={generations},target=\"{target}\",tag=\"{tag}\"")
         experiment_cmd += f" run_experiment.sh\n"
 
         target_file.write(experiment_cmd)
