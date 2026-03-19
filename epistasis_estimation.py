cf4af438fad5a0a563fa06ea4dbb04dd0597f45e
Christoph Stein <christoph.werner.stein@gmail.com>
2026-03-19 13:21:58 +0100

    Adjust epistasis result visualization

--- epistasis_estimation.py
+++ epistasis_estimation.py
@@ -5,7 +5,7 @@ from quasim import Circuit, get_unitary
 import random
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score as compute_r2_score
-from statistics import mean, stdev
+from statistics import mean, stdev, median
 from tqdm import tqdm
 import warnings
 
@@ -58,66 +58,41 @@ def estimate_r2_score(qubit_num: int, gate_count: int, population_size: int, see
 if __name__ == "__main__":
 
     seed_num = 30
-    qubit_num = 4
-    gate_count = 10
     population_size = 10_000
 
     gate_set = CLIFFORD_PLUS_T
 
-    # Vary gate counts, keep qubit num fixed
-    qubit_num = 4
-    gate_counts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    ax = plt.subplot()
 
-    r2_scores_per_gate_count = []
-    for gate_count in tqdm(gate_counts, desc="Gates"):
+    qubit_nums = [2, 4, 6]
+    gate_counts = [1, 2, 3, 4, 5 , 6, 7, 8, 9, 10]
 
-        r2_scores = []
-        for seed in tqdm(range(seed_num), total=seed_num, leave=False):
-            r2_score = estimate_r2_score(
-                qubit_num=qubit_num, gate_count=gate_count, population_size=population_size, seed=seed)
-            r2_scores.append(r2_score)
-
-        r2_scores_per_gate_count.append(r2_scores)
-
-    fig, ax = plt.subplots()
-    ax.boxplot(r2_scores_per_gate_count,
-               tick_labels=gate_counts)
-
-    plt.xlabel("gate count")
-    plt.ylabel("r2")
-    plt.ylim(0)
-
-    plt.grid()
-
-    plt.savefig(
-        f"results/epistasis_{qubit_num}q_varying_g.png", bbox_inches='tight')
-    plt.clf()
-
-    # Vary qubit num, keep gate count fixed
-    gate_count = 5
-    qubit_nums = [1, 2, 3, 4, 5, 6, 7]
-
-    r2_scores_per_qubit_num = []
     for qubit_num in tqdm(qubit_nums, desc="Qubits"):
 
-        r2_scores = []
-        for seed in tqdm(range(seed_num), total=seed_num, leave=False):
-            r2_score = estimate_r2_score(
-                qubit_num=qubit_num, gate_count=gate_count, population_size=population_size, seed=seed)
-            r2_scores.append(r2_score)
+        r2_scores_per_gate_count = []
 
-        r2_scores_per_qubit_num.append(r2_scores)
+        for gate_count in tqdm(gate_counts, desc="Gates", leave=False):
 
-    fig, ax = plt.subplots()
-    ax.boxplot(r2_scores_per_qubit_num,
-               tick_labels=qubit_nums)
+            r2_scores = []
+            for seed in tqdm(range(seed_num), total=seed_num, leave=False):
+                r2_score = estimate_r2_score(
+                    qubit_num=qubit_num, gate_count=gate_count, population_size=population_size, seed=seed)
+                r2_scores.append(r2_score)
 
-    plt.xlabel("qubit count")
+            r2_scores_per_gate_count.append(median(r2_scores))
+
+        ax.plot(gate_counts, r2_scores_per_gate_count,
+                label=f"{qubit_num} qubits", linewidth=1)
+
+    plt.xticks(gate_counts)
+    plt.xlabel("gate count")
+
     plt.ylabel("r2")
     plt.ylim(0)
 
     plt.grid()
+    plt.legend()
 
     plt.savefig(
-        f"results/epistasis_varying_q_{gate_count}g.png", bbox_inches='tight')
+        f"results/epistasis.png", bbox_inches='tight')
     plt.clf()
