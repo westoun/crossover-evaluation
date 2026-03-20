7861b8d6c86e5ae7eef7461469eb444f73652d5c
Christoph Stein <christoph.werner.stein@gmail.com>
2026-03-20 11:11:36 +0100

    Replace r2 with adjusted r2 score

--- epistasis_estimation.py
+++ epistasis_estimation.py
@@ -14,7 +14,7 @@ from core.gate_sets import CLIFFORD_PLUS_T, CLIFFORD_PLUS_T_PLUS_I
 from core.fitness import Fitness, AbsoluteDistanceFitness
 
 
-def estimate_r2_score(qubit_num: int, gate_count: int, population_size: int, seed: int = 0) -> float:
+def estimate_adjusted_r2_score(qubit_num: int, gate_count: int, population_size: int, seed: int = 0) -> float:
     random.seed(seed)
     np_random.seed(seed)
 
@@ -52,42 +52,45 @@ def estimate_r2_score(qubit_num: int, gate_count: int, population_size: int, see
         y_pred = regressor.predict(X)
 
     r2_score = compute_r2_score(y_true=y, y_pred=y_pred)
-    return r2_score
+
+    adjusted_r2_score = 1 - (1 - r2_score) * (len(data) - 1) / (len(data) - len(X.columns) - 1)
+    return adjusted_r2_score
 
 
 if __name__ == "__main__":
 
     seed_num = 30
-    population_size = 10_000
+    population_size = 20_000
 
     gate_set = CLIFFORD_PLUS_T
 
     ax = plt.subplot()
 
     qubit_nums = [2, 4, 6]
-    gate_counts = [1, 2, 3, 4, 5 , 6, 7, 8, 9, 10]
+    gate_counts = [1, 3, 5, 7, 9] #,  6, 7, 8, 9, 10]
 
     for qubit_num in tqdm(qubit_nums, desc="Qubits"):
 
-        r2_scores_per_gate_count = []
+        adjusted_r2_scores = []
 
         for gate_count in tqdm(gate_counts, desc="Gates", leave=False):
 
-            r2_scores = []
+            gate_count_scores = []
             for seed in tqdm(range(seed_num), total=seed_num, leave=False):
-                r2_score = estimate_r2_score(
+                adjusted_r2_score = estimate_adjusted_r2_score(
                     qubit_num=qubit_num, gate_count=gate_count, population_size=population_size, seed=seed)
-                r2_scores.append(r2_score)
+                
+                gate_count_scores.append(adjusted_r2_score)
 
-            r2_scores_per_gate_count.append(median(r2_scores))
+            adjusted_r2_scores.append(median(gate_count_scores))
 
-        ax.plot(gate_counts, r2_scores_per_gate_count,
+        ax.plot(gate_counts, adjusted_r2_scores,
                 label=f"{qubit_num} qubits", linewidth=1)
 
     plt.xticks(gate_counts)
     plt.xlabel("gate count")
 
-    plt.ylabel("r2")
+    plt.ylabel("adjusted R^2")
     plt.ylim(0)
 
     plt.grid()
