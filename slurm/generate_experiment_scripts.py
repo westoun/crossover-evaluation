08813869e9f151984af12a9411e35c3ee548e89f
Christoph Stein <christoph.werner.stein@gmail.com>
2026-03-23 09:46:24 +0100

    Adjust mutation prob of mutation only experiments

--- slurm/generate_experiment_scripts.py
+++ slurm/generate_experiment_scripts.py
@@ -1,8 +1,8 @@
 seed_num = 30
 seed_offset = 30
 
-mutation_prob = 0.04
-crossover_prob = 0.7
+mutation_prob = 0.03
+crossover_prob = 0.3
 
 generations = 20_000
 
@@ -33,8 +33,8 @@ with open("slurm/run_experiments.sh", "w") as target_file:
         target_file.write(experiment_cmd)
 
         # Mutation only
-        experiment_cmd = f"sbatch --job-name=exp_0.04mp0.0cp_one-point_{seed}s"
-        experiment_cmd += (f" --export=crossover=\"one-point\",mutation_prob=0.04,crossover_prob=0.0,"
+        experiment_cmd = f"sbatch --job-name=exp_{mutation_prob}mp0.0cp_one-point_{seed}s"
+        experiment_cmd += (f" --export=crossover=\"one-point\",mutation_prob={mutation_prob},crossover_prob=0.0,"
                             f"seed={seed},result_dir=/home/ws/ws16/CEIQ/results,generations={generations},target=\"{target}\",tag=\"{tag}\"")
         experiment_cmd += f" run_experiment.sh\n"
 
